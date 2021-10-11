530a33e4ac1dc25d80b0cbbd8effc114bb2b0ded
amorgan659 <78758498+amorgan659@users.noreply.github.com>
2021-10-11 10:36:01 -0400

    Add files via upload

---
+++ chngcolor.js
@@ -0,0 +1,15 @@
+function setPurple() {
+	document.body.style.background = "Purple";
+}
+function setBlue() {
+	document.body.style.background = "Blue";
+}
+function setRed() {
+	document.body.style.background = "Red";
+}
+function setPink() {
+  document.body.style.background = "Pink";
+}
+function setGreen() {
+  document.body.style.background = "Green";
+}
